8d890bcc4eff40b0ad625f1a07db783fd48819ac
Nick Halvorsen <lingosalad@gmail.com>
2017-12-02 20:25:24 -0500

    tested works okay

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-PGUSER=etherslam PGPASSWORD=icedtea PGHOST=localhost PGDATABASE=etherslam node load-transaction.js 1000000
+PGUSER=etherslam PGPASSWORD=icedtea PGHOST=localhost PGDATABASE=etherslam node load-transaction.js 1 100000
