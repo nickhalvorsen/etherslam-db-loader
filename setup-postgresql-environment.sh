e3b05f5b143ed1b8c585943c80833ccfda96165b
Nick Halvorsen <lingosalad@gmail.com>
2017-12-11 20:07:19 -0500

    refactored PGSQL environment vars in start script

---
+++ setup-postgresql-environment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PGUSER=etherslam 
+export PGPASSWORD=icedtea 
+export PGHOST=localhost 
+export PGDATABASE=etherslam
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-latest=$(PGUSER=etherslam PGPASSWORD=icedtea PGHOST=localhost PGDATABASE=etherslam node get-latest-block.js)
-PGUSER=etherslam PGPASSWORD=icedtea PGHOST=localhost PGDATABASE=etherslam node load-transaction.js $latest 9000000 
+
+source setup-postgresql-environment.sh
+latest=$(node get-latest-block.js)
+node load-transaction.js $latest 9000000 
